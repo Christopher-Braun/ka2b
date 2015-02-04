0c5cc237bbae935d28887f80677aa76e2165e123
chribra1 <cbraun@gmx.li>
2015-02-04 16:35:49 +0100

    sioehe unten

---
+++ Ka2CloudService/Ka2WebRole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ka2WebRole.MvcApplication" Language="C#" %>
---
+++ Ka2CloudService/Ka2WebRole/Models/TagModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc4WebRole.Models
+{
+    public class TagModel
+    {
+        public TagModel()
+        {
+            ID = Guid.NewGuid();
+            Recipes = new List<RecipeModel>();
+        }
+
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        public virtual List<RecipeModel> Recipes { get; set; }
+
+        [Required]
+        [Display(Name = "Tag")]
+        public String Caption { get; set; }
+
+    }
+}
