6c846b345c2644f098b3e81ba9b34b12ec03542d
chribra1 <cbraun@gmx.li>
2015-02-04 16:35:23 +0100

    Solution getauscht

---
+++ Ka2CloudService/Ka2WebRole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ka2WebRole.Startup))]
+namespace Ka2WebRole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
